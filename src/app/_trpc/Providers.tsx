50f706a0218edfc6c7fa78285dd60139c4b2731e
hiyoungaroo <hiyoungaroo@gmail.com>
2023-10-09 04:39:36 +0900

    trpc httpBatchLink

--- src/app/_trpc/Providers.tsx
+++ src/app/_trpc/Providers.tsx
@@ -11,7 +11,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: '/src/app/api/trpc',
+          url: '/api/trpc',
         }),
       ],
     })
